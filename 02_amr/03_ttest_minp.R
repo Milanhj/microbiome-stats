737951840a51c94cbea166d6eac57d20546345ad
Milan Hilde-Jones <milanhilde-jones2023@u.northwestern.edu>
2025-06-09 10:32:37 -0700

    Redo ttest functions to get foldchages

--- 02_amr/03_ttest_minp.R
+++ 02_amr/03_ttest_minp.R
@@ -36,23 +36,63 @@ for (i in 5:ncol(counts)){
   # if not a longitudinal study, use seperate datasets for pre.post intervention min-p calculation
 # log2: should the data be log2+e transformed? log2(MLS + 1/total_reads*1000000)
 # list: do you want the results to be returned in list format?
+
+# dat: data with a group variable, total reads variable, and timepoint variable
+# group_col: column with grouping variable
+# tot_reads_col: column index with total reads number for that sample
+# start_col: column index where count data starts
+# time_col: column index with timepoint variable
+# list: logical, list == TRUE returns results in list format, FALSE for single tibble
+# log2: if TRUE, log2 transforms data for the t-test
+
 ttest_log2rm <- function(dat, group_col, tot_reads_col, start_col, time_col,
                          list = FALSE, log2 = TRUE){
   
-  # Store variable (e.g. drug class names)
-  rm_names <- colnames(dat)[start_col:length(colnames(dat))]
+  get_lm_results <- function(dat, formula) {
+    # Model Output
+    res <- summary(lm(formula, data = dat))
+    
+    # Tibble to store lm output for all covariates
+    store_result <- tibble(
+      var = NA,
+      estimate = NA,
+      SE = NA, 
+      p_value = NA)
+    
+    # Organize lm output for all covariates, remove intercept
+    for (i in 2:nrow(res$coefficients)){
+      store_result <- rbind(
+        store_result,
+        tibble(
+          var = rownames(res$coefficients)[i],
+          estimate = res$coefficients[i, "Estimate"],
+          SE = res$coefficients[i,"Std. Error"],
+          p_value = res$coefficients[i,"Pr(>|t|)"]
+        )
+      )
+      # Remove the first row (with the NAs)
+      if (i == 2)  store_result <- store_result[-1,]
+    } # for i
+    return(store_result)
+  } # end function
   
-  # Change column names to generic names
+  rm_names <- colnames(dat)[start_col:length(colnames(dat))]
   colnames(dat)[tot_reads_col] <- "total_reads"
-  colnames(dat)[group_col] <- "group"
+  colnames(dat)[group_col] <- "tx"
   colnames(dat)[time_col] <- "timepoint"
   
   if(list){ # Store in a list
     result <- list()
   }else{ # Store in a single table
-    result <- tibble(timepoint = NA, var = NA, pval = NA)
+    result <- tibble(
+      timepoint = NA, 
+      name = NA,
+      var = NA, 
+      estimate = NA,
+      SE = NA,
+      p_value = NA
+    )
   }
-  # Loop for class
   for (i in 1:length(rm_names)){ # t-test for each time for class i
     
     # The index for class i
@@ -65,76 +105,91 @@ ttest_log2rm <- function(dat, group_col, tot_reads_col, start_col, time_col,
       dplyr::rename(untrans = !!sym(rm_names[i]))
     
     # Store vector of times for class i
-    times <- sort(unique(as.character(dat_log2$timepoint)))
+    times <- sort(unique(dat_log2$timepoint))
     
-    # Create a matrix to hold t-test results for each timepoint
-    mttest <- matrix(ncol = 3, nrow = length(times),
-                     dimnames = list(NULL, c("timepoint", "var", "pval")))
-    # Loop for time
-    for (j in 1:length(times)){
+    # Create a tibble to hold lm t-test results for each timepoint
+    lm_tib <- tibble(
+      timepoint = NA, 
+      name = NA,
+      var = NA, 
+      estimate = NA,
+      SE = NA,
+      p_value = NA
+    )
+    for (t in 1:length(times)){
       # Filter for just the correct timepoint
       dat_tp <- dat_log2 %>% 
-        filter(timepoint == times[j])
-      # Store time and class name
-      mttest[j,1] <- times[j]
-      mttest[j,2] <- rm_names[i]
+        filter(timepoint == times[t])
+      
       # T-test with either the original variable or the log2 transformed
-      if (log2){ # log2 
-        mttest[j,3] <- t.test(log2 ~ group, data = dat_tp)$p.value
-      } else{ # raw data
-        mttest[j,3] <- t.test(untrans ~ group, data = dat_tp)$p.value
-      }
-    } # j
-    
-    # Store Results
-    if(list){ # Store in a list
-      result[[i]] <- as_tibble(mttest) %>% 
-        mutate(
-          timepoint = as.numeric(timepoint),
-          pval = as.numeric(pval)
+      if (log2){
+        lm_tib <- rbind(
+          lm_tib, 
+          as_tibble(cbind(
+            tibble(
+              timepoint = times[t],
+              name = rm_names[i]),
+            get_lm_results(dat_tp, as.formula(log2 ~ tx))
+          ))
         )
+      } else{
+        
+        lm_tib <- rbind(
+          lm_tib, 
+          as_tibble(cbind(
+            tibble(
+              timepoint = times[t],
+              name = rm_names[i]),
+            get_lm_results(dat_tp, as.formula(untrans ~ tx))
+          ))
+        )
+        
+      } # if else
+      if (t == 1) lm_tib <- lm_tib[-1,]
+    } # t
+    
+    if(list){ 
+      # Store in a list
+      result[[i]] <- lm_tib
     }else{ # Store in a single table
       # Bind results from class i to the overall result table
-      result <- rbind(result,
-                      as_tibble(mttest) %>% 
-                        mutate(
-                          timepoint = as.numeric(timepoint),
-                          pval = as.numeric(pval)
-                        ))
+      result <- rbind(result,lm_tib)
       # If it's the first iteration, take off the top row with NA used to initialize
-      if(i == 1){result <- result[-1,]}
+      if(i == 1){result <- result[-1,]
+      }
     }
+    
   } # i
+  if(log2){
+    colnames(result)[4] <- "log2estimate"
+    colnames(result)[5] <- "log2SE"
+  }
   return(result) 
 } # end function
+
+
 
 
 # Runs ttest_log2rm() and Min-p for multiple comparisons
 # B: number of permutations
 ttest_min_p <- function(dat, B = 999, group_col, tot_reads_col, 
-                        start_col, time_col, 
+                        start_col, time_col,
                         list = FALSE, log2 = TRUE){
   
   # Make sure group column is named tx
   colnames(dat)[group_col] <- "tx"
   
-  # Sort and store timepoints
-  times <- sort(as.numeric(unique(as.vector(unlist(dat[,time_col])))))
-  
   # Vector of class names
   rM_names <- colnames(dat)[c(start_col:ncol(dat))]
   
   # Call function to compute observed pvalues
-  observed_result_table <- ttest_log2rm(
+  observed_result <- ttest_log2rm(
     dat = dat, group_col = group_col, 
     tot_reads_col = tot_reads_col, 
     start_col = start_col, time_col = time_col,
     list = list, log2 = log2
   )
-  # Store observed P-values
-  observed_p_values <- observed_result_table$pval
-  # Extract observed minimum P-value
-  observed_min_p <- min(observed_p_values, na.rm = TRUE)
+  observed_p_values <- observed_result$p_value
   
   # Storage for permuted minimum P-values (numeric vector)
   min_perm_p_values <- numeric(B)
@@ -151,30 +206,30 @@ ttest_min_p <- function(dat, B = 999, group_col, tot_reads_col,
       tot_reads_col = tot_reads_col, 
       start_col = start_col, time_col = time_col,
       list = list, log2 = log2
-    )
-    # Store minimum P-value from vector of permuted p-values
-    min_perm_p_values[b] <- min(perm_p_values$pval, na.rm = TRUE)
+    )$p_value
+    
+    # Record minimum P-value from vector of permuted p-values
+    min_perm_p_values[b] <- min(perm_p_values, na.rm = TRUE)
     
   } # for b
   
-  # Tibble to hold adjusted p-values
+  # Adjust P-values
   m_padj <- tibble(
-    timepoint = rep(times, length(rM_names)),
-    var = observed_result_table$var,
+    timepoint = observed_result$timepoint,
+    name = observed_result$name,
     minp_adj = rep(0)
   )
-  # Adjust P-values
   for (i in 1:length(observed_p_values)) {
     # Proportion of permuted min p values less than or equal to the observed p-value i
     m_padj[i,3] <- mean(min_perm_p_values <= observed_p_values[i])
     
   } # for i
   
-  # Join to raw p-value from observed results
-  padj_result_table <- observed_result_table %>% 
-    left_join(m_padj, by = c("var", "timepoint"))
+  minp_result <- observed_result %>% 
+    left_join(m_padj, by = c("name", "timepoint"))
   
-  return(padj_result_table)
+  # Return results
+  return(minp_result)
 } # end function
 
 
